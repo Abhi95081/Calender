6f666060f7d6b42be2e2f624b9093ba847c5707c
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-08-04 11:36:54 +0530

    MainActivity.kt commit

--- app/src/main/java/com/example/calender/MainActivity.kt
+++ app/src/main/java/com/example/calender/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.calender
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +13,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.calender.data.PanchangProvider
 import com.example.calender.ui.theme.CalenderTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PanchangProvider.loadFromAssets(this) // <-- ✅ Load JSON here
         enableEdgeToEdge()
         setContent {
             CalenderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CalendarScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalenderTheme {
-        Greeting("Android")
-    }
-}
